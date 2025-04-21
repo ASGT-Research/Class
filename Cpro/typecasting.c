aad8079c3a76294d775b5269aa797eecf6a5c4f8
ASGT-Research <avsinghrathotft@gmail.gom>
2025-04-22 02:13:28 +0530

    Cpro

---
+++ Cpro/typecasting.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+ int x=97;
+  for (x=97;x<=122;x++) {
+   printf("%c\n",x);
+  }
+  return 0;
+}
---
+++ DSA/stack.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define MAX 20
+#include<stdlib.h>
+#include<string.h>
+int stack[MAX];
+char Stack[MAX];
+char infix[MAX];
+char Postfix[MAX];
+int top = -1;
+
+int main(){
+  
+}
