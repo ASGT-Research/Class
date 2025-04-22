f4871a5d5d55e5a24cbb45adc109e494ceb16c73
ASGT-Research <shivamsinghft8@gmail.com>
2025-04-22 15:02:20 +0530

    collage

--- DSA/stack.c
+++ DSA/stack.c
@@ -1,13 +1,1 @@
-#include <stdio.h>
-#define MAX 20
-#include<stdlib.h>
-#include<string.h>
-int stack[MAX];
-char Stack[MAX];
-char infix[MAX];
-char Postfix[MAX];
-int top = -1;
-
-int main(){
-  
-}
+#include<stdio.h>
